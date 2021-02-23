6ee305bfd9b2e7ea502c5522257209b03eb40965
Sara Hult <sarahult00@gmail.com>
2021-02-23 10:47:13 +0100

    Vill försöka skicka Japan

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   title = 'Isaks';
 
   constructor() {
-
+    country: "Japan";
   }
 
   getFood() {
--- app/src/app/drinks/drinks.component.ts
+++ app/src/app/drinks/drinks.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-drinks',
@@ -10,6 +10,6 @@ export class DrinksComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+  } 
 
 }
