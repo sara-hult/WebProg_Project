b39232423cbe8a333fd7e761c79c323b1ccbbd0a
nesdust <hannes.sonesson@gmail.com>
2021-02-23 14:30:20 +0100

    Lagt till httpClient i app.module

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DrinksComponent } from './drinks/drinks.component';
@@ -16,7 +18,8 @@ import { FoodComponent } from './food/food.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
