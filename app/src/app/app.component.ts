83f10a17d9f2167d9b658acd6cdd0c0a1c9e5436
Sara Hult <sarahult00@gmail.com>
2021-02-23 10:25:14 +0100

    Skapade skalet till 3 getfunktioner

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Isaks';
+
+  constructor() {
+
+  }
+
+  getFood() {
+    console.log("Food");
+  }
+
+  getDrinks() {
+    console.log("Drinks");
+  }
+
+  getMovies() {
+    console.log("Movies");
+
+  }
+
 }
