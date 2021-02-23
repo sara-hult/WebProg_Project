f251e60caaf09097395b37b49e405f7ca99dcd50
nesdust <hannes.sonesson@gmail.com>
2021-02-23 16:39:17 +0100

    en till kommentar

--- app/src/app/drinks/drinks.component.ts
+++ app/src/app/drinks/drinks.component.ts
@@ -22,4 +22,4 @@ export class DrinksComponent implements OnInit {
   }
 }
 
-//document.getElementsByClassName("css-1wo4jfn")[0].outerText För att få dryckesinfo från systembolaget
+//document.getElementsByClassName("css-1wo4jfn")[0].outerText För att få dryckesinfo från systembolaget hejehjeje
