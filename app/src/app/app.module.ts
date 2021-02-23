4c379e7fc09b2ca6a2109798769c3c3a26f08e28
Sara Hult <sarahult00@gmail.com>
2021-02-23 09:55:47 +0100

    Skapat komponenterna food, drinks och movies

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DrinksComponent } from './drinks/drinks.component';
+import { MoviesComponent } from './movies/movies.component';
+import { FoodComponent } from './food/food.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrinksComponent,
+    MoviesComponent,
+    FoodComponent
   ],
   imports: [
     BrowserModule,
