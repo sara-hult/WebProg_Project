6e8bd85f2fd523946a2224e1fbdc2f9d309ccf05
nesdust <hannes.sonesson@gmail.com>
2021-02-23 16:09:42 +0100

    skriver ut drink från CocktailDB

--- app/src/app/drinks/drinks.component.ts
+++ app/src/app/drinks/drinks.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Countries } from '../../util/countries';
-import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-drinks',
   templateUrl: './drinks.component.html',
-  styleUrls: ['./drinks.component.css']
+  styleUrls: ['./drinks.component.css'],
+
 })
 export class DrinksComponent implements OnInit {
   @Input() country!: Countries;
-  constructor() { }
+  hej: any;
+  constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
-  } 
-
+  ngOnInit() {
+    // Simple GET request with response type <any>
+    this.http.get<any>('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=campari beer').subscribe(data => {
+      console.log(data);
+    })
+  }
 }
 
 //document.getElementsByClassName("css-1wo4jfn")[0].outerText För att få dryckesinfo från systembolaget
