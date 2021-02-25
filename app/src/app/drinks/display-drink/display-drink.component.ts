df62670c53b9f15bfb1e79a52c34690c06c3eb34
nesdust <hannes.sonesson@gmail.com>
2021-02-25 10:49:21 +0100

    subcomponents added to drink

---
+++ app/src/app/drinks/display-drink/display-drink.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-drink',
+  templateUrl: './display-drink.component.html',
+  styleUrls: ['./display-drink.component.css']
+})
+export class DisplayDrinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
