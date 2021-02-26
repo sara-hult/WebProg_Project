2e7147a9b62df89243ac9cbf3937b14f2342f986
sirnacl <emil.gedenryd@gmail.com>
2021-02-26 15:14:39 +0100

    Choose country in landing

--- app/src/app/landing/landing.component.ts
+++ app/src/app/landing/landing.component.ts
@@ -1,19 +1,23 @@
 import { map } from 'rxjs/operators';
 import { Countries } from './../../util/countries';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'Landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
+
 })
 export class LandingComponent implements OnInit {
 
   availableCountries: string[];
+  chosenCountryString:string;
   chosenCountry:Countries = Countries.USA;
 
-  constructor() {
+  constructor(private _router: Router) {
     this.availableCountries = this.getAvailableCountries();
+    this.chosenCountryString = this.availableCountries[0];
   }
 
   ngOnInit(): void {
@@ -24,7 +28,24 @@ export class LandingComponent implements OnInit {
   }
 
   submit(){
+    let country:Countries | undefined = this.getCountryFromName(this.chosenCountryString);
+    if(country !== undefined){
+      this._router.navigate(["overview/", country]);
+    }else{
+      throw new Error("Country Undefined!");
+    }
+  }
 
+  getCountryFromName(countryName: string): Countries | undefined {
+    let country: Countries | undefined = undefined;
+    Object.entries(Countries).forEach(
+    ([key, value]) => {
+      if(key === countryName){
+        country = value;
+      }
+    }
+  );
+    return country;
   }
 
   selectChangeHandler(event: any){
