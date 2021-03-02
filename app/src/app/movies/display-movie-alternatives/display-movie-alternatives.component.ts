36da3b19b937ad05a16994ad42abd9e2ec8cc93a
sirnacl <emil.gedenryd@gmail.com>
2021-03-02 11:22:47 +0100

    import fix

--- app/src/app/movies/display-movie-alternatives/display-movie-alternatives.component.ts
+++ app/src/app/movies/display-movie-alternatives/display-movie-alternatives.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Movie } from './../../../util/movie';
+import { Movie } from '../../../util/movie';
 
 @Component({
   selector: 'DisplayMovieAlternatives',
